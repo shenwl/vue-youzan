5846f989c7c79dc85cd1bbb605596fe6fae47f57
shenwl <shenweilun96@gmail.com>
2018-08-16 19:41:07 +0800

    webpack配置修改为多页面配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -11,10 +11,11 @@ function resolve (dir) {
 
 
 module.exports = {
-  context: path.resolve(__dirname, '../'),
-  entry: {
-    app: './src/main.js'
-  },
+  // context: path.resolve(__dirname, '../'),
+  // entry: {
+  //   app: './src/main.js'
+  // },
+  entry: utils.entries();
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
