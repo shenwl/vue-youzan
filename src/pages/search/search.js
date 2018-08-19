b8cf1500c38377bc54edb9ded986e3419d2193a6
shenwl <shenweilun96@gmail.com>
2018-08-19 21:57:08 +0800

    商品列表渲染

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,2 +1,33 @@
 import 'css/common.css';
 import './search.css';
+
+import Vue from 'vue';
+import axios from 'axios';
+import url from 'js/api';
+import qs from 'qs';
+
+var {keyword, id} = qs.parse(window.location.search.substr(1));
+
+var appSearch = new Vue({
+  el: '#app-search',
+  data: {
+    keyword: keyword,
+    searchList: null,
+  },
+  created() {
+    this.getSearchList()
+  },
+  methods: {
+    getSearchList() {
+      axios.post(url.searchList, {keyword, id}).then(res => {
+        this.searchList = res.data.lists;
+        console.log(this.searchList)
+      })
+    },
+  },
+  filters: {
+    priceNumber(price) {
+      return parseFloat(price).toFixed(2)
+    }
+  }
+})
