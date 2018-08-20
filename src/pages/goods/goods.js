09560e08171a544b7072e00b2c8cc52770c857ab
shenwl <shenweilun96@gmail.com>
2018-08-20 22:28:50 +0800

    init goods page

---
+++ src/pages/goods/goods.js
@@ -0,0 +1,19 @@
+import './goods_common.css';
+import './goods_custom.css';
+import './goods.css';
+import './goods_theme.css';
+import './goods_mars.css';
+import './goods_sku.css';
+
+import Vue from 'vue';
+import url from 'js/api';
+import axios from 'axios';
+import mixin from 'js/mixin'
+import qs from 'qs';
+
+
+var appGoods = new Vue({
+  el: '#app-goods',
+})
+
+
